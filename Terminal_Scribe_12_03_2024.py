85a81f0e3c9a2bf8f8a68b055dd4b7ea5cde03a2
zaheer.emeran <z.emeran27@gmail.com>
2024-03-11 22:13:13 -1200

    Made it excessive

--- Terminal_Scribe_12_03_2024.py
+++ Terminal_Scribe_12_03_2024.py
@@ -1,5 +1,7 @@
 from tkinter import *
-#this is random code
+import numpy as np
+
+# this is random code
 root = Tk()
 root.title('Zee - Terminal Scribe')
 root.geometry("500x500")
@@ -7,10 +9,39 @@ tech_Container = Canvas(root, width=500, height=500, bg="white")
 tech_Container.pack(pady=20)
 
 
-square_Size = input("Input: ")
-tech_Container.create_rectangle(0, 0, square_Size, square_Size, fill="blue")
-root.mainloop()
+# For the fun of it
+# Choose amount of squares (max 3)
+# Make the user choose between ten colours for each square
+# Max Val = 500
+# Min Val = ?
+
+def colour_Selector(Input):
+    print("hello")
 
 
+colour_Of_Squares = [None] * 5
 
+print("Select The Amount of Squares (1,2,3)")
+total_Squares = (input("Square Input: "))
+square_Size = (input("Input Size For Main Square: "))
 
+if total_Squares.isdigit() and int(total_Squares) <= 3:
+    var = int(square_Size)
+    for i in range(0, int(total_Squares)):
+        colour_Input_Temp = input(f"Colour Input For Square {i + 1}. (R) Red, (G) Green, (B) Blue: ")
+        if colour_Input_Temp == "R":
+            colour_Of_Squares[i] = "Red"
+
+        elif colour_Input_Temp == "G":
+            colour_Of_Squares[i] = "Green"
+
+        elif colour_Input_Temp == "B":
+            colour_Of_Squares[i] = "Blue"
+
+        tech_Container.create_rectangle(10, 10, var, var, fill = colour_Of_Squares[i])
+        var = var / 2
+
+    root.mainloop()
+
+else:
+    print("Invalid Value!")
